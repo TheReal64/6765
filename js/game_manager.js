1d5e6912200b650b3276be8ea1b5d5dc2c0a1a33
Ryan2144 <70672438+Ryan2144@users.noreply.github.com>
2020-11-30 20:03:15 -0600

    Get the 6765 tile

--- js/game_manager.js
+++ js/game_manager.js
@@ -72,8 +72,7 @@ GameManager.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    var rand = Math.random();
-    var value = rand < 0.80 ? 1 : rand < 0.90 ? 2 : 3;
+    var value = Math.random() < 0.82 ? 1 : 2;
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
@@ -182,8 +181,8 @@ GameManager.prototype.move = function (direction) {
           // Update the score
           self.score += merged.value;
 
-          // The mighty 2048 tile
-          if (merged.value === 987) self.won = true;
+          // The mighty 6765 tile
+          if (merged.value === 6765) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }
